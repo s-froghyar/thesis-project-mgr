3a6cf1b8abf320cb05e1a89f8f11c7bbe292bf40
TheSoma300 <froghyar.soma@gmal.com>
2021-04-10 19:34:19 +0100

    adding that extra layer

--- config/models/segmented_cnn.py
+++ config/models/segmented_cnn.py
@@ -8,8 +8,10 @@ class SegmentedCNN(nn.Module):
         self.conv1 = nn.Conv2d(1, 64, kernel_size=5, stride=1, padding=(1, 1))
         self.conv2 = nn.Conv2d(64, 64, kernel_size=5, stride=1, padding=(1, 1))
 
-        self.fc1 = nn.Linear(15360, 10)
+        self.fc1 = nn.Linear(15360, 500)
+        self.fc2 = nn.Linear(500, 10)
         nn.init.kaiming_normal_(self.fc1.weight, mode='fan_in')
+        nn.init.kaiming_normal_(self.fc2.weight, mode='fan_in')
         self.fc1.bias.data.fill_(0.01)
 
         self.pool = F.max_pool2d
@@ -22,5 +24,6 @@ class SegmentedCNN(nn.Module):
         x = F.relu(self.pool(self.conv2(x), 2))
         x = x.reshape(x.shape[0], -1)
         x = self.dropout(F.relu(self.fc1(x)))
+        x = self.fc2(x)
 
         return self.log_sm(x)
